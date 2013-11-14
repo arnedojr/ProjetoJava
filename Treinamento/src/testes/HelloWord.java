c2a3e310834b1afd3e28d438b510a64be2232540
Arnedo <Arnedo@191.168.17.104>
2013-11-14 18:12:30 -0300

    commit do primeiro projeto

---
+++ Treinamento/src/testes/HelloWord.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package testes;
+
+import java.io.ObjectInputStream.GetField;
+
+/**
+ * @author Arnedo
+ * 
+ */
+public class HelloWord {
+	private String hh;
+ 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub  ctrl+O para resource
+		System.out.println("hello!!");
+		System.out.println("novo teste");
+		//int a = 1;
+
+		int a = 0;
+		switch (a) {
+		case 1:
+			
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	public String getHh() {
+		return hh;
+	}
+
+	public void setHh(String hh) {
+		this.hh = hh;
+	}
+
+}
